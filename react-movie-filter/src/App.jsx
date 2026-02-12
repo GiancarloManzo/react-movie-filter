3f5fc720c2875b5faabbb60798b819d2e78d77c9
Giancarlo <cooper458.gm@gmail.com>
2026-02-12 16:20:39 +0100

    added export function

--- react-movie-filter/src/App.jsx
+++ react-movie-filter/src/App.jsx
@@ -6,3 +6,19 @@ const movies = [
   { title: "Interstellar", genre: "Fantascienza" },
   { title: "Pulp Fiction", genre: "Thriller" },
 ];
+
+export default function App() {
+  return (
+    <div>
+      <h1>Movie Filter</h1>
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.title}>
+            {movie.title} - {movie.genre}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
